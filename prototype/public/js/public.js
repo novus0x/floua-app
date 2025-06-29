ec44997390082a306360d9cc02156aa112928663
novus0x <novus0x@proton.me>
2025-06-29 10:16:44 +0000

    Updating mobile view

--- prototype/public/js/public.js
+++ prototype/public/js/public.js
@@ -1,3 +1,74 @@
+/********************** Media Queries **********************/
+window.addEventListener("resize", () => {
+    const window_size = window.innerWidth;
+
+    /*** Sidebar controller ***/
+    const icon = document.getElementById("sidebar-toggle-icon");
+    const sidebar = document.getElementById("sidebar");
+    const content = document.getElementById("content");
+
+    function reset_sidebar() {
+        if (sidebar.classList.contains("sidebar-hide")) sidebar.classList.remove("sidebar-hide");
+        if (content.classList.contains("content-shide")) content.classList.remove("content-shide");
+    }
+
+    /*** Actions ***/
+    if (window_size <= 600) {
+
+    } else if (window_size <= 768) {
+
+    } else if (window_size <= 992) {
+
+    } else if (window_size <= 1200) {
+        /* Sidebar controller */
+        if (icon.textContent == "notes") icon.textContent = "menu";
+        reset_sidebar();
+    } else {
+        /* Sidebar controller */
+        if (icon.textContent == "menu") icon.textContent = "notes";
+        reset_sidebar();
+    }
+});
+
+/********************** Search **********************/
+const search_btn = document.getElementById("search-btn");
+const search_backdrop = document.getElementById("search-backdrop");
+const search_container = document.getElementById("search-results");
+const search_result_input = document.getElementById("search-result-input");
+
+document.getElementById("search-result-input")?.addEventListener("input", (e) => {
+    if (e.target.value == "") search_container.classList.add("hidden");
+    else {
+        if (search_container.classList.contains("hidden")) search_container.classList.remove("hidden");
+        
+        const link_input = document.getElementById("search-result-link-input");
+        link_input.textContent = e.target.value;
+    }
+});
+
+document.getElementById("search-result-input")?.addEventListener("focusout", (e) => {
+    setTimeout(() => {
+        search_container.classList.add("hidden");
+    }, 150);
+});
+
+search_btn?.addEventListener("click", () => {
+    search_backdrop.classList.toggle("hidden");
+    search_result_input.classList.toggle("show");
+    search_result_input.classList.toggle("scale-in-center");
+});
+
+search_backdrop?.addEventListener("click", () => {
+    search_result_input.classList.remove("scale-in-center");
+    search_result_input.classList.toggle("scale-out-center");
+
+    setTimeout(() => {
+        search_backdrop.classList.toggle("hidden");
+        search_result_input.classList.toggle("show");
+        search_result_input.classList.remove("scale-out-center");
+    }, 500);
+})
+
 /********************** Sidebar controller **********************/
 document.getElementById("sidebar-button")?.addEventListener("click", () => {
     const icon = document.getElementById("sidebar-toggle-icon");
@@ -12,6 +83,9 @@ document.getElementById("sidebar-button")?.addEventListener("click", () => {
 });
 
 /********************** Sign in and Sign up controllers **********************/
+const navbar_actions = document.getElementById("navbar-actions");
+const sidebar_actions = document.getElementById("sidebar-actions");
+
 const modal_container = document.getElementById("modal");
 const modal_close = document.getElementById("modal-close");
 const modal_actions = document.getElementById("modal-actions");
@@ -55,29 +129,47 @@ const sign_up_dom = `
 <button class="btn modal-input-button">Sign up</button>
 `;
 
-document.getElementById("signin-button")?.addEventListener("click", () => {    
+function user_modal(type) {
     // Content
-    modal_actions.innerHTML = sign_in_dom;
-    modal_title.textContent = "Sign in";
-    modal_actions.action = "#signin"
+    if (type == "signin") {
+        modal_actions.innerHTML = sign_in_dom;
+        modal_title.textContent = "Sign in";
+        modal_actions.action = "#signin"
+    } else if (type == "signup") {
+        modal_actions.innerHTML = sign_up_dom;
+        modal_title.textContent = "Sign up";
+        modal_actions.action = "#signup"
+    }
 
     // Animation
     modal_container.classList.toggle("hidden");
     modal_content.classList.add("slide-in-top")
     modal_content.classList.remove("slide-out-top");
-});
+}
 
-document.getElementById("signup-button")?.addEventListener("click", () => {
-    // Content
-    modal_actions.innerHTML = sign_up_dom;
-    modal_title.textContent = "Sign up";
-    modal_actions.action = "#signup"
+navbar_actions?.addEventListener("click", (e) => {
+    const btn = e.target.closest(".modal-btn");
 
-    // Animation
-    modal_container.classList.toggle("hidden");
-    modal_content.classList.add("slide-in-top")
-    modal_content.classList.remove("slide-out-top");
-});
+    if (btn) {
+        console.log("ok");
+        const modal_type = btn.getAttribute("data-modal");
+        if (modal_type == "signin") user_modal(modal_type);
+        else if (modal_type == "signup") user_modal(modal_type);
+    }
+
+})
+
+sidebar_actions?.addEventListener("click", (e) => {
+    const btn = e.target.closest(".modal-btn");
+
+    if (btn) {
+        console.log("ok");
+        const modal_type = btn.getAttribute("data-modal");
+        if (modal_type == "signin") user_modal(modal_type);
+        else if (modal_type == "signup") user_modal(modal_type);
+    }
+
+})
 
 modal_close?.addEventListener("click", () => {
     modal_content.classList.remove("slide-in-top")
